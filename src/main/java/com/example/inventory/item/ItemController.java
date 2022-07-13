5f056358f28af200dab57ea157078476465891d6
sflankkila <simonflankkila@gmail.com>
2022-07-13 21:46:59 +0300

    Added GET, POST, PUT and DELETE operations. Next step is to test everything.

--- src/main/java/com/example/inventory/item/ItemController.java
+++ src/main/java/com/example/inventory/item/ItemController.java
@@ -1,9 +1,12 @@
 package com.example.inventory.item;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Controller for {@link Item}
@@ -25,5 +28,47 @@ public class ItemController {
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.ok().body("pong");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> findAll() {
+        final List<Item> items = itemService.findAll();
+        return ResponseEntity.ok().body(items);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> find(@PathVariable("id") final Long id) {
+        final Optional<Item> item = itemService.find(id);
+        return ResponseEntity.of(item);
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> create(@RequestBody Item item) {
+        final Item created = itemService.create(item);
+        final URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping
+    public ResponseEntity<Item> update(@PathVariable("id") final Long id,
+            @RequestBody final Item updatedItem) {
+        final Optional<Item> updated = itemService.update(id, updatedItem);
+
+        return updated.map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    final Item created = itemService.create(updatedItem);
+                    final URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                            .path("/{id}").buildAndExpand(created.getId()).toUri();
+                    return ResponseEntity.created(location).body(created);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Item> delete(@PathVariable("id") final Long id) {
+        itemService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
